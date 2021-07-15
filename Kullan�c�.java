0a3de9243d3b62dcb65a1912007a3dfb7e3a15e6
ranadudukabak <77778044+ranadudukabak@users.noreply.github.com>
2021-07-15 13:44:41 +0300

    Update Kullan�c�.java

--- Kullan�c�.java
+++ Kullan�c�.java
@@ -1,28 +1,21 @@
-
 package prolab_proje_2;
 
 import static prolab_proje_2.Test.kullaniciKartlar;
 
-
 public class Kullanıcı extends Oyuncu {
     
-    
-
     public Kullanıcı(int oyuncuID, String oyuncuAdi, int oyuncuSkor) {
         super(oyuncuID, oyuncuAdi, oyuncuSkor);
     }
     
-
     static int randomtut;
     static int randtut;
 
     public static int randomyakala(){
         return randomtut;
     }
+    
     public static int randyakala(){
         return randtut;
     }
-
-
-    
 }
